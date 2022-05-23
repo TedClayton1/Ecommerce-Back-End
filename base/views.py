6dec665418103ab62d1be3cf135488bae55dd349
TedClayton1 <ted_clayton@hotmail.com>
2022-05-23 11:47:07 -0400

    Attempted to add products page to backend but getting AttributeError: module base.views has no attribute getProducts.

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
 
 
-def getRoutes(request):    
-    return JsonResponse('Hello', safe=False)
+def getRoutes(request): 
+    routes = [        
+    ]  
+    return JsonResponse(routes, safe=False)
+
+    def getProducts(request):
+        return JsonResponse(products, safe=False)
